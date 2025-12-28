0b50e1e9c086aa1284c5888e6d564ae08350bd0e
Will Bush <git@willbush.dev>
2025-12-27 19:40:43 -0600

    crkbd: formatting

--- keyboards/crkbd/rev1/keymaps/willbush/keymap.c
+++ keyboards/crkbd/rev1/keymaps/willbush/keymap.c
@@ -1,6 +1,14 @@
 #include QMK_KEYBOARD_H
 
-enum crkbd_layers { _COLEMAK, _LOWER, _HYPER, _RAISE, _FN, _NUM, _ADJUST };
+enum crkbd_layers {
+  _COLEMAK,
+  _LOWER,
+  _HYPER,
+  _RAISE,
+  _FN,
+  _NUM,
+  _ADJUST
+};
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
@@ -25,42 +33,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT,    KC_Z,   ALT_X,    KC_C,    KC_D,    KC_V,                      KC_K,    KC_H,    KC_Y, ALT_DOT, KC_SLSH, SFT_ENT,
                                         KC_LGUI,   LOWER,   HYPER,  KC_SPC,   RAISE,  KC_RALT
   ),
-
   [_LOWER] = LAYOUT_split_3x6_3(
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     KC_LCTL, KC_TILD, KC_LPRN, KC_EXLM,  KC_EQL, KC_RPRN,                   KC_LCBR, KC_PERC, KC_AMPR, KC_RCBR, KC_PIPE,  KC_DEL,
     _______, XXXXXXX,   KC_LT,   KC_GT, XXXXXXX,      FN,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, KC_RSFT,
                                         XXXXXXX, _______, XXXXXXX, _______,  ADJUST, _______
   ),
-
   [_HYPER] = LAYOUT_split_3x6_3(
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     KC_LCTL,  KC_GRV, KC_LBRC, KC_PLUS, KC_MINS, KC_RBRC,                   KC_CIRC, KC_ASTR, KC_HASH,  KC_DLR, KC_BSLS,  KC_DEL,
     _______, XXXXXXX,   KC_LT,   KC_GT, KC_UNDS,     NUM,                   XXXXXXX,   KC_AT, XXXXXXX, KC_RALT, XXXXXXX, KC_RSFT,
                                         XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, _______
   ),
-
   [_RAISE] = LAYOUT_split_3x6_3(
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     KC_LCTL, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN,  KC_END,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX,  KC_DEL,
     _______, XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, KC_RSFT,
                                         XXXXXXX,  ADJUST, _______, _______, _______, _______
   ),
-
   [_FN] = LAYOUT_split_3x6_3(
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    KC_F12,   KC_F7,   KC_F8,   KC_F9, XXXXXXX, XXXXXXX,
     KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    KC_F11,   KC_F4,   KC_F5,   KC_F6, XXXXXXX,  KC_DEL,
     _______, XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX,                    KC_F10,   KC_F1,   KC_F2,   KC_F3, XXXXXXX, KC_RSFT,
                                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
   ),
-
   [_NUM] = LAYOUT_split_3x6_3(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,    KC_7,    KC_8,    KC_9, KC_MINS, _______,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,    KC_4,    KC_5,    KC_6, KC_PLUS, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, KC_RSFT,
                                         XXXXXXX, XXXXXXX, _______, _______,    KC_0,  KC_DOT
   ),
-
   [_ADJUST] = LAYOUT_split_3x6_3(
     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
