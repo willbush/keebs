ca9cb700499d99be66f12d08166169995704d5a9
Will Bush <git@willbush.dev>
2026-07-27 08:14:31 -0500

    planck: add SC2 layers

--- keyboards/planck/rev6/keymaps/willbush/keymap.c
+++ keyboards/planck/rev6/keymaps/willbush/keymap.c
@@ -2,7 +2,9 @@
 
 enum planck_layers {
   _COLEMAK,
-  _GAME,
+  _SC2, // SC2 game layer
+  _CG,  // SC2 control groups
+  _CGS, // SC2 control groups with shift held (append)
   _LOWER,
   _HYPER,
   _RAISE,
@@ -12,8 +14,10 @@ enum planck_layers {
 };
 
 #define COLEMAK PDF(_COLEMAK)
-#define GAME PDF(_GAME)
+#define SC2 PDF(_SC2)
 #define LOWER MO(_LOWER)
+#define CG MO(_CG)
+#define CGS MO(_CGS)
 #define RAISE MO(_RAISE)
 #define HYPER MO(_HYPER)
 #define FN MO(_FN)
@@ -36,12 +40,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT,    KC_Z,   ALT_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_Y, ALT_DOT, KC_SLSH, SFT_ENT,
     XXXXXXX,     NUM, XXXXXXX, KC_LGUI,   LOWER,   HYPER,  KC_SPC,   RAISE, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
   ),
-  [_GAME] = LAYOUT_planck_grid(
+  // *** SC2 game layouts starts ***
+  [_SC2] = LAYOUT_planck_grid(
      KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U, KC_COMM, KC_SCLN, KC_BSPC,
     CTL_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O, CTL_QOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_Y,  KC_DOT, KC_SLSH, SFT_ENT,
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT,   LOWER,   HYPER,  KC_SPC,   RAISE, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT,      CG,     CGS,  KC_SPC,   RAISE, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
   ),
+  [_CG] = LAYOUT_planck_grid(
+       KC_0,    KC_1,    KC_2,    KC_3,  KC_SPC, _______, _______, _______, _______, _______, _______, _______,
+    KC_LCTL,    KC_4,    KC_5,    KC_6,  KC_SPC, _______, _______, _______, _______, _______, _______,  KC_DEL,
+    _______,    KC_7,    KC_8,    KC_9,  KC_SPC,      FN, _______, _______, _______, _______, _______, KC_RSFT,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+  [_CGS] = LAYOUT_planck_grid(
+    S(KC_0), S(KC_1), S(KC_2), S(KC_3),  KC_SPC, _______, _______, _______, _______, _______, _______, _______,
+    KC_LCTL, S(KC_4), S(KC_5), S(KC_6),  KC_SPC, _______, _______, _______, _______, _______, _______,  KC_DEL,
+    _______, S(KC_7), S(KC_8), S(KC_9),  KC_SPC, _______, _______, _______, _______, _______, _______, KC_RSFT,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+  // *** SC2 layouts ends ***
   [_LOWER] = LAYOUT_planck_grid(
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     KC_LCTL, KC_TILD, KC_LPRN, KC_EXLM,  KC_EQL, KC_RPRN, KC_LCBR, KC_PERC, KC_AMPR, KC_RCBR, KC_PIPE,  KC_DEL,
@@ -74,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_ADJUST] = LAYOUT_planck_grid(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX,   AU_ON,  AU_OFF, XXXXXXX, XXXXXXX, COLEMAK,    GAME, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX,   AU_ON,  AU_OFF, XXXXXXX, XXXXXXX, COLEMAK,     SC2, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DB_TOGG, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX
   )
@@ -94,5 +112,11 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  // The SC2 layer swaps LOWER for CG, so it needs its own path to _ADJUST.
+  // Must come after the call above, which clears _ADJUST.
+  if (layer_state_cmp(state, _CG) && layer_state_cmp(state, _RAISE)) {
+    state |= (layer_state_t)1 << _ADJUST;
+  }
+  return state;
 }
